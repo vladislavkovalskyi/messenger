1b573d3597802af0efbb6a4bbfcaf158fb5f6a7a
vladislavkovalskyi <v.darknesssb@gmail.com>
2023-09-30 05:12:07 +0300

    Made obtaining information about the display resolution

--- main.go
+++ main.go
@@ -2,10 +2,8 @@ package main
 
 import (
 	"messenger/gui"
-	"messenger/tools"
 )
 
 func main() {
-	println(tools.GetScreenResolution())
 	gui.Application()
 }
--- tools/screen.go
+++ tools/screen.go
@@ -1,7 +1,31 @@
 package tools
 
-// mb troubles
-//func GetScreenResolution() (int, int) {
-//	screenWidth, screenHeight := robotgo.GetScreenSize()
-//	return screenWidth, screenHeight
-//}
+import (
+	"fmt"
+	"syscall"
+)
+
+var (
+	user32           = syscall.NewLazyDLL("user32.dll")
+	getSystemMetrics = user32.NewProc("GetSystemMetrics")
+)
+
+const (
+	ScreenX = 0
+	ScreenY = 1
+)
+
+func GetScreenResolution(scale float32) (float32, float32, error) {
+	// default param
+	if scale == .0 {
+		scale = 1.0
+	}
+
+	screenWidth, _, _ := getSystemMetrics.Call(ScreenX)
+	screenHeight, _, _ := getSystemMetrics.Call(ScreenY)
+
+	if screenWidth == 0 || screenHeight == 0 {
+		return 0, 0, fmt.Errorf("error getting screen resolution")
+	}
+	return float32(screenWidth) * scale, float32(screenHeight) * scale, nil
+}
